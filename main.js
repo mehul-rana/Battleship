0f382fa080d99026882d9281cf10771f54bbfae1
Mehul Rana <mehulrana412@gmail.com>
2024-10-05 20:50:57 -0500

    updated the "stats" variable to show the accuracy as a percentage

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let location1 = 3;
-let location2 = 4;
-let location3 = 5;
+let location1 = Math.floor(Math.random()*5);
+let location2 = location1 + 1;
+let location3 = location2 + 1;
 
 let guess;
 let hits = 0;
@@ -8,16 +8,31 @@ let guesses = 0;
 
 let isSunk = false;
 
-while(isSunk === false){
-  guess = prompt('Ready, aim, fire! (enter a number from 0-6):')
+while (isSunk == false) {
+  guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
 
-  if(guess < 0 || guess > 6){
-    alert('Please enter a valid cell number!')
-  }else{
-    guesses++
+  if (guess < 0 || guess > 6) {
+    alert("Please enter a valid cell number!");
+  } else {
+    guesses++;
 
-    if(guess === location1 || guess === location2 || guess === location3){
-      hits++
+    if (guess == location1 || guess == location2 || guess == location3) {
+      alert('HIT!')
+      hits++;
+
+      if (hits == 3) {
+        isSunk = true;
+
+        alert("You sank my battleship!");
+      }
+    }else{
+      alert('MISS!')
     }
   }
 }
+
+let stats = "You took " + guesses + "to sink the battleship, which means your shooting accuracy was " + ((3/guesses)*100) + '%'
+
+// `You took ${guesses} guesses to sink the battleship, which means your shooting accuracy was 3/${guesses}`
+
+alert(stats);
